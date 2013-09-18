7dfc862bbfdf2c7afcf48f222ad196dc7131792b
biplab <biplab@Saswatis-MacBook-Air.local>
2013-09-17 23:22:15 -0700

    1. Adding the new file Procfile and system.properties to make it work on heroku 2. Adding the updated Application.java file

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,19 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application extends Controller {
   
+    public static List<String> menuList = new ArrayList<String>();
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        menuList.add("Biography");
+        menuList.add("Photos");
+        menuList.add("Resume");
+        menuList.add("Interests");
+        return ok(index.render(menuList));
     }
-  
+
 }
