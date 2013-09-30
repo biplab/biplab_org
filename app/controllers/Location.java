f2add859a97b63c047f10ad9277233b1a9e355e5
biplab <biplab@gmail.com>
2013-09-29 20:46:07 -0700

    Adding link to location field with google maps api

--- app/controllers/Location.java
+++ app/controllers/Location.java
@@ -6,15 +6,10 @@ import views.html.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Application extends Controller {
+public class Location extends Controller {
   
-    public static List<String> menuList = new ArrayList<String>();
     public static Result index() {
-        menuList.add("Biography");
-        menuList.add("Photos");
-        menuList.add("Resume");
-        menuList.add("Interests");
-        return ok(index.render(menuList));
+        return ok(location.render());
     }
 
 }
